aa372438e3e78de9f4dc1951cc23cca476d0b4f7
LoganLipke <lolipke@gmail.com>
2020-10-11 21:26:29 -0700

    Made some changes

--- ciss247/assgn1/Decrypt.c
+++ ciss247/assgn1/Decrypt.c
@@ -17,12 +17,29 @@ int main (int argc, char *argv[])
 {
     char line[8];
     unsigned int bits;
-
+    unsigned key1, key2;
+    // Step 1 Convert to integer
+    if (argc == 2)
+        get_keys(argv[1], key1, key2);
+    else
+    {
+        puts("Correct usage: ./decrypt {key} {ciphertext}");
+        exit(1);
+    }
+    
     while (fgets(line, 8, stdin))
     {
         sscanf(line, "%x", &bits);
-        printf("%x", bits);
+        shuffle_nibbles(&bits);
     }
+    printf("\n%x %x\n", key1, key2);
+
+    // Step 3 XOR with key2
+    // Shuffle nibbles
+    // XOR with key1
+    // Rotate each septet left 3
+    // Convert septets to chars
+
     return 0;
 }
 
@@ -30,6 +47,7 @@ int main (int argc, char *argv[])
 void get_keys (char *twokeys, unsigned *key1, unsigned *key2)
 {
 	//extracts two 28-bit keys from 4 bytes
+    
     return;
 }
 
@@ -40,18 +58,20 @@ unsigned int get_n_bits (unsigned bits, int width, int index)
     * width is the number of bits to be extracted
     * index * width is the index of the rightmost bit to extract
     */
-   return 0;
+   return bits;
 }   
 
 unsigned int rotate_left3(unsigned bits)
 {    
     // given a sequence of 7 bits, rotate 3 bits to the left
-    return 0;
+    
+    return bits;
 }
 
 void shuffle_nibbles (unsigned *bits)
 {	
     //shuffle the 7 nibbles according to the pattern
+    printf("%x\n", *bits % 16);
 }
 
 void decode_28bits (unsigned cipher, char *plain, unsigned key1, unsigned key2)
