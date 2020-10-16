9c6254216f7216b38245f36887733bc491d12516
LoganLipke <lipkel2@wwu.edu>
2020-10-15 19:24:21 -0700

    Added lab3 definitions

---
+++ ciss247/lab3/lab3.c
@@ -0,0 +1,51 @@
+/*
+*   Lab3
+*   Logan Lipke CISS 247 Fall
+*   10/15/20
+*/
+
+#include <stdio.h>
+
+int func1(int x, int y); // Computes the bit-wise AND of two integers, using only the ~ and |
+int func2(int x, int y); // Computes the bit-wise exclusive-or of two integers, using only the ~ and & operators
+int func3(int x); // Returns 1 if any odd-numbered bit in an integer is set to 1, and 0 otherwise
+int func4(int x); // Returns 1 if all the odd-numbered bits in an integer are set to 1, and 0 otherwise
+int func5(int highbit, int lowbit); // Returns a bit-mask with all 1's between lowbit and highbit, inclusive
+int func6(int x, int m, int n); // swaps the mth byte and the nth byte of an integer
+
+int main()
+{
+
+
+    return 0;
+}
+
+int func1(int x, int y)
+{
+    return 0;
+}
+
+int func2(int x, int y)
+{
+    return 0;
+}
+
+int func3(int x)
+{
+    return 0;
+}
+
+int func4(int x)
+{
+    return 0;
+}
+
+int func5(int highbit, int lowbit)
+{
+    return 0;
+}
+
+int func6(int x, int m, int n)
+{
+    return 0;
+}
