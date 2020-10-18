c5080553f6ae378cec82d216097dca22b93bfe43
LoganLipke <lipkel2@wwu.edu>
2020-10-17 18:28:35 -0700

    Added extra tests

--- ciss247/lab3/bits.c
+++ ciss247/lab3/bits.c
@@ -16,8 +16,10 @@ int func6(int x, int m, int n); // swaps the mth byte and the nth byte of an int
 int main()
 {
     printf("func1(5,6) should return %x. Result = %x \n", func1(5,6), 5 & 6);
+    printf("func1(10,21) should return %x. Result = %x \n", func1(10,21), 10 & 21);
 
     printf("func2(4,5) should return %x. Result = %x \n", func2(4,5), 4 ^ 5);
+    printf("func2(42,13) should return %x. Result = %x \n", func2(42,13), 42 ^ 13);
 
     printf("func3(0x5) should return 0. Result = %x\n", func3(0x5));
     printf("func3(0x7) should return 1. Result = %x\n", func3(0x7));
@@ -53,16 +55,4 @@ int func4(int x)
     // Returns 1 if all the odd-numbered bits in an integer are set to 1, and 0 otherwise
     int tempBits = ((x >> 24) & 0xaa) & ((x >> 16) & 0xaa) & ((x >> 8) & 0xaa) & (x & 0xaa);
     return ((tempBits >> 1) & 1) & ((tempBits >> 3) & 1) & ((tempBits >> 5) & 1) & ((tempBits >> 7) & 1);
-}
-
-int func5(int highbit, int lowbit)
-{
-    // Returns a bit-mask with all 1's between lowbit and highbit, inclusive
-    return 0;
-}
-
-int func6(int x, int m, int n)
-{
-    // swaps the mth byte and the nth byte of an integer
-    return 0;
 }
