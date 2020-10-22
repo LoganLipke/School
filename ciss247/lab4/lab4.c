815d73f0c401f520856ea8ed191827bbe3495e4d
LoganLipke <lipkel2@wwu.edu>
2020-10-22 13:54:01 -0700

    Added some function implementations

--- ciss247/lab4/lab4.c
+++ ciss247/lab4/lab4.c
@@ -8,26 +8,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ADDR 50
+#define MAX_LINES 50
 #define NUM_FIELDS 5
-#define SIZE_FIELDS 10
+#define SIZE_FIELDS 6
 
 
-char memory[MAX_ADDR][NUM_FIELDS][SIZE_FIELDS];
+char memory[MAX_LINES][NUM_FIELDS][SIZE_FIELDS];
 int pc = 200;
-enum Registers{X0, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, XZR=0};
-int regArray[11];
+enum Registers{XZR, X0, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10};
+int regArray[12];
 
-void parseFile(char* fileName, int *opCount, int *memCount, char memory[MAX_ADDR][NUM_FIELDS][SIZE_FIELDS]);
-// void ADD(struct Operation *op);
-// void ADDI(struct Operation *op);
-// void SUB(struct Operation *op);
-// void LDUR(struct Operation *op);
-// void STUR(struct Operation *op);
-// int B(struct Operation *op);
-// int CBZ(struct Operation *op);
+void parseFile(char* fileName, int *opCount, int *memCount, char memory[MAX_LINES][NUM_FIELDS][SIZE_FIELDS]);
+void ADD(char * dest, char * src1, char * src2);
+void ADDI(char * dest, char * src1, char * src2);
+void SUB(char * dest, char * src1, char * src2);
+void LDUR(char * dest, char * src1, char * src2);
+void STUR(char * dest, char * src1, char * src2);
+void B(char * dest, char * src1, char * src2);
+void CBZ(char * dest, char * src1, char * src2);
 int convertReg(char *reg);
 int convertSrc(char *src1, char *src2);
+int convertIntermed(char *intermed);
+int convertOp(char *op);
 
 int main(int argc, char* argv[])
 {
@@ -41,6 +43,32 @@ int main(int argc, char* argv[])
     {
         i = ((pc - 200) /4) + memCount;
         printf("%d : %s %s, %s, %s\n", pc, memory[i][1], memory[i][2], memory[i][3], memory[i][4]);
+        
+        switch (convertOp(memory[i][1]))
+        {
+        case 1:
+            ADD(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        case 2:
+            ADDI(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        case 3:
+            SUB(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        case 4:
+            LDUR(memory[i][2], memory[i][3], memory[i][4]);
+            convertSrc(memory[i][3], memory[i][4]);
+            break;
+        case 5:
+            STUR(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        case 6:
+            B(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        case 7:
+            CBZ(memory[i][2], memory[i][3], memory[i][4]);
+            break;
+        }
         pc += 4;
     }
 
@@ -49,18 +77,17 @@ int main(int argc, char* argv[])
 }
 
 
-void parseFile(char* fileName, int *opCount, int *memCount, char memory[MAX_ADDR][NUM_FIELDS][SIZE_FIELDS])
+void parseFile(char* fileName, int *opCount, int *memCount, char memory[MAX_LINES][NUM_FIELDS][SIZE_FIELDS])
 {
     char buff[255];
     FILE *fp = fopen(fileName, "r");
     while (fgets(buff, 50, (FILE *)fp))
     {
-        char memAddr[8];
-        char op[8];
-        char dest[8], src1[8], src2[8];
+        char memAddr[SIZE_FIELDS];
+        char op[SIZE_FIELDS];
+        char dest[SIZE_FIELDS], src1[SIZE_FIELDS], src2[SIZE_FIELDS];
         sscanf(buff, "%s %s %[^,], %[^,], %s\n", memAddr, op, dest, src1, src2);
-        
-        // convertSrc(src1, src2);
+
         strcpy(memory[*opCount+*memCount][0], memAddr);
         strcpy(memory[*opCount+*memCount][1], op);
         strcpy(memory[*opCount+*memCount][2], dest);
@@ -104,44 +131,71 @@ int convertReg(char *reg)
         return -1;
 }
 
-// int convertSrc(char *src1, char *src2)
-// {
+int convertSrc(char *src1, char *src2)
+{
+    char tmp1[5], tmp2[5];
+    sscanf(src1, "[%s", tmp1);
+    sscanf(src2, "%[^]]", tmp2);
+
     
-//     printf("TEST %s %s\n", src1, src2);
-//     sscanf(src1, "", );
-// }
+}
 
-// void ADD(struct Operation *op)
-// {
+int convertIntermed(char *intermed)
+{
+    char temp[5];
+    sscanf(intermed, "#%s", temp);
+    return atoi(temp);
+}
 
-// }
+int convertOp(char *op)
+{
+        if (strcmp(op, "ADD") == 0)
+        return 1;
+    else if (strcmp(op, "ADDI") == 0)
+        return 2;
+    else if (strcmp(op, "SUB") == 0)
+        return 3;
+    else if (strcmp(op, "LDUR") == 0)
+        return 4;
+    else if (strcmp(op, "STUR") == 0)
+        return 5;
+    else if (strcmp(op, "B") == 0)
+        return 6;
+    else if (strcmp(op, "CBZ") == 0)
+        return 7;
+}
 
-// void ADDI(struct Operation *op)
-// {
+void ADD(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// }
+void ADDI(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// void SUB(struct Operation *op)
-// {
+void SUB(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// }
+void LDUR(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// void LDUR(struct Operation *op)
-// {
+void STUR(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// }
+void B(char * dest, char * src1, char * src2)
+{
+    return;
+}
 
-// void STUR(struct Operation *op)
-// {
-
-// }
-
-// int B(struct Operation *op)
-// {
-
-// }
-
-// int CBZ(struct Operation *op)
-// {
-
-// }
+void CBZ(char * dest, char * src1, char * src2)
+{
+    return;
+}
