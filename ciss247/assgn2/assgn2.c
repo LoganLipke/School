ea84a41857deafdc91e3da1100a5dfc003bfac3d
LoganLipke <lolipke@gmail.com>
2020-11-07 16:50:36 -0800

    Made more additions

--- ciss247/assgn2/assgn2.c
+++ ciss247/assgn2/assgn2.c
@@ -26,6 +26,13 @@ int main(int argc, char *argv[])
     FILE *input = fopen(argv[1], "r");
     int i = 0;
     
+    if (argc != 2)
+    {
+        puts("Please supply a filename");
+        puts("Ex: ./assgn2 wordsEn.txt");
+        exit(1);
+    }
+
     while (fgets(wordlist[i], MAX_WORD_LEN, (FILE *)input))
     {
         char test[MAX_WORD_LEN];
@@ -40,51 +47,63 @@ int main(int argc, char *argv[])
     {            
         int wordCount = 0;
         int subCount = 0;
+        int minHamming = 100;
+        char closest[MAX_WORD_LEN];
         printf("Your word: \n\t%s\n", word);
+        for (int j = 0; j < i; j++)
+        {
+            if ((getHamming(word, wordlist[j]) < minHamming) && (strlen(word) == strlen(wordlist[j]))) // Gets lowest hamming score to find closest word
+            {
+                minHamming = getHamming(word, wordlist[j]);
+                strcpy(closest, wordlist[j]);
+            }
+            
+            if ((wordCount < 5) && (strlen(word) == strlen(wordlist[j])) && (getHamming(word, wordlist[j]) < strlen(word)) && (strcmp(word, wordlist[j]) != 0))
+            {   // Finds 5 alternatives to chosen word
+                if (wordCount == 0)
+                    printf("Potential alternatives: \n");
+                wordCount++;
+                printf("Score: %d\t%s \n", getHamming(word, wordlist[j]), wordlist[j]);
+            }
+        }
         for (int j = 0; j < i; j++)
         {
             if ((subCount < 5) && (checkSubstring(word, wordlist[j])))
             {
                 if (subCount == 0)
-                    printf("Word found in: \n");
+                    printf("\nWord found in: \n");
                 subCount++;
                 printf("\t%s \n", wordlist[j]);
             }
-
-            if ((wordCount < 5) && (strlen(word) == strlen(wordlist[j])) && (getHamming(word, wordlist[j]) < strlen(word)))
-            {
-                if (wordCount == 0)
-                    printf("Potential alternatives: \n");
-                wordCount++;
-                printf("\t%s \n", wordlist[j]);
-            }
         }
+        printf("\nClosest word found : \nScore: %d\t%s\n", minHamming, closest);
         getWord(word);
     }
 }
 
 void toLower(char *word)
-{
+{   // Changes given word to lowercase
     for (int i = 0; i < strlen(word); i++)
     {
         if (word[i] >= 'A' && word[i] <= 'Z')
             word[i] = word[i] + 32;
-            
+
         if (word[i] == '\n')
             word[i] = '\0';
     }
 }
 
 int getHamming(char *word, char *word2)
-{
+{   // Determines Hamming Distance for two words
     int hamming = 0;
     for (int i = 0; i < strlen(word); i++)
+    {
         if (word[i] != word2[i])
         {
-            if (i == 0 && (word[i] != word2[i])) // Gives higher precedence to words starting with the same letter
+            if (i == 0 && (word[i] != word2[i])) // Gives lower score to words starting with the same letter
                 hamming += 5;
 
-            if (word[i] - word2[i] > 7 || word2[i] - word[i] > 7) // Gives precedence to words whose letters are closer in the alphabet
+            if (word[i] - word2[i] > 10 || word2[i] - word[i] > 10) // Gives lower score to words whose letters are closer in the alphabet
                 hamming += 3;
 
             if ((isVowel(word[i]) && isVowel(word2[i])) || (!isVowel(word[i]) && !isVowel(word2[i]))) // Gives lower score to letters that are both consonants or vowels
@@ -92,11 +111,12 @@ int getHamming(char *word, char *word2)
             else
                 hamming += 2;
         }
+    }
     return hamming;
 }
 
 void getWord(char *word)
-{
+{   // Gets input as word
     printf("Word to check: ");
     fgets(word, MAX_WORD_LEN, stdin);
     toLower(word);
@@ -104,6 +124,7 @@ void getWord(char *word)
 
 int isVowel(char c)
 {
+    // Checks if a character is a vowel
     const char *vowels = "aeiou";
     for (int i = 0; i < 5; i++)
         if (c == vowels[i])
