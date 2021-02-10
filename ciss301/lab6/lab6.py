9746e2884af2c086dadaf287f2eabedec8193998
LoganLipke <lipkel2@wwu.edu>
2021-02-10 13:51:56 -0800

    Finished?

--- ciss301/lab6/lab6.py
+++ ciss301/lab6/lab6.py
@@ -20,31 +20,41 @@ def is_odd(n):
 
 
 def set_equal(set_a, set_b):
+    target = len(set_b)
+    count = 0
+    flag = False
     for i in set_a:
         if not isinstance(i, list) and i not in set_b:
             return False
         elif isinstance(i, list):
             for j in set_b:
                 if isinstance(j, list):
-                    set_subset(i, j)
+                    if set_subset(i, j):
+                        return True
+        else:
+            flag = True
+            if i in set_b:
+                count += 1
+    if count == target and flag:
+        return True
+    else: 
+        return False
 
 def set_subset(sub_a, sub_b):
-    print(sub_a, sub_b)
     for i in sub_a:
-        print("sub", i, sub_b)
         if not isinstance(i, list) and i not in sub_b:
             return False
         elif isinstance(i, list):
             for j in sub_b:
                 if isinstance(j, list):
-                    set_equal(i, j)
-        else:
-            return True
-
+                    if set_equal(i, j):
+                        return True
 
 
 def main():
     print(set_equal([1,[2,[3,4]]],[[[4,3],2],1]))
+    print(set_equal([1,[2,[3,4]]],[[4,3,2],1]))
+    
     
 if __name__ == "__main__":
     main()
