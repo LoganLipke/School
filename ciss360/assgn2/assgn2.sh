0131e2cae3f3972b17f1ab346101448c76397597
LoganLipke <lipkel2@wwu.edu>
2021-02-17 12:44:36 -0800

    Made progress

--- ciss360/assgn2/assgn2.sh
+++ ciss360/assgn2/assgn2.sh
@@ -4,30 +4,52 @@ displayProcesses () {
     clear
     echo "Press any key to quit"
     while [ true ] ; do
-        read -t 10 -n 1
+        read -t 6 -n 1
         if [ $? = 0 ] ; then
             exit ;
         else
-            echo '===================================='
             clear
-            procList=`ls /proc | grep [[:digit:]]`
-            for process in $procList
+            echo '============================================'
+            procList=`ls /proc | grep [[:digit:]] | sort -n`
+            for process in $procList 
             do  
-                processName=`head /proc/$process/status -n 1`
-                printf "PID: %s Name: %s\n" $process $processName
-                
+                processName=`head /proc/$process/status -n 1 2> /dev/null`
+                if [ $? = 0 ]; then
+                    printf "PID: %s \t%s\n" "$process" "$processName"
+                fi
             done
+            echo '============================================'
             echo "Press any key to quit"
         fi
     done
 }
 
 watchProcess () {
-    break
+    clear
+    echo "Press any key to quit"
+    while [ true ] ; do
+        read -t 6 -n 1
+        if [ $? = 0 ] ; then
+            exit ;
+        else
+            clear
+            echo '============================================'
+            procList=`ls /proc | grep [[:digit:]] | sort -n`
+            for process in $procList 
+            do  
+                processName=`head /proc/$process/status -n 1 2> /dev/null`
+                if [ $? = 0 ]; then
+                    printf "PID: %s \t%s\n" "$process" "$processName"
+                fi
+            done
+            echo '============================================'
+            echo "Press any key to quit"
+        fi
+    done
 }
 
 parseOptions () {
     break
 }
 
-displayProcesses
+watchProcess
